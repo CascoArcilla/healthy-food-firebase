03ee50d975864c68fb399b86233472cd08c7e03f
CascoArcilla <normon1230@gmail.com>
2023-07-22 08:52:19 -0600

    test:fixed error when loading the login in e2e

--- cypress/e2e/Login.cy.ts
+++ cypress/e2e/Login.cy.ts
@@ -1,9 +1,12 @@
-describe('When a user wants to login with email/password', () => {
+describe('When a user wants to login', () => {
   beforeEach(() => {
-    cy.visit('/login');
+    cy.visit('/');
+    cy.get('[data-cy="nav-login"]').should('be.visible');
+    cy.wait(300);
+    cy.get('[data-cy="nav-login"]').click();
   });
 
-  it('Then the list of post should be shown', () => {
+  it('Then the list of post should be shown', async () => {
     cy.url().should('include', '/login');
     cy.get('[data-cy="login-form"]').should('be.visible');
     cy.get('[data-cy="email-field"]').type('user@example.com');
