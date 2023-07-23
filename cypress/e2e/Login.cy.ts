e13bda65e229b99922ade06e69647361cad3675e
CascoArcilla <normon1230@gmail.com>
2023-07-22 21:02:30 -0600

    test:e2e login change the cy.visit of beforeEach

--- cypress/e2e/Login.cy.ts
+++ cypress/e2e/Login.cy.ts
@@ -1,17 +1,15 @@
 describe('When the user login', () => {
   beforeEach(() => {
-    cy.visit('/');
-    cy.get('[data-cy="nav-login"]').should('be.visible');
+    cy.visit('/login');
     cy.wait(300);
-    cy.get('[data-cy="nav-login"]').click();
   });
 
   it('Then the list of post should be shown', () => {
-    cy.url().should('include', '/login');
+    cy.url().should('include', '/login');    
     cy.get('[data-cy="login-form"]').should('be.visible');
     cy.get('[data-cy="email-field"]').type('user@example.com');
     cy.get('[data-cy="password-field"]').type('password');
     cy.get('[data-cy="login-button"]').click();
-    cy.url().should('equal', 'http://localhost:3000/');
+    cy.url().should("equal", "http://localhost:3000/");
   });
 })
